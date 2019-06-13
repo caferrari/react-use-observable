bd707459b64c382e12df7306da56342dca756624
Carlos André Ferrari <caferrari@gmail.com>
2019-06-13 12:58:00 -0300

    fixed imports

--- src/observable.ts
+++ src/observable.ts
@@ -1,17 +1,7 @@
 import { DependencyList, useCallback, useEffect, useState } from 'react';
 import { Observable } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
 
 type observerFunction<T> = () => Observable<T>
-type equal = (a: any, b: any) => boolean;
-
-let isEqual: equal = (a: any, b: any) => JSON.stringify(a) === JSON.stringify(b);
-
-import('lodash.isequal')
-  .then(module => module.default)
-  .catch(() => import ('lodash').then(module => module.isEqual))
-  .catch(() => isEqual)
-  .then(fnt => isEqual = fnt)
 
 export function useObservable<T>(
   observableGenerator: observerFunction<T>,
@@ -26,9 +16,6 @@ export function useObservable<T>(
 
   useEffect(() => {
     const sub = cb()
-      .pipe(
-        distinctUntilChanged((a, b) => isEqual(a, b))
-      )
       .subscribe(
         (data: T) => {
           setValue(data);
