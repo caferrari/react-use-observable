3e2134fc0d930438f0282947c509fed01aa4c504
Carlos André Ferrari <caferrari@gmail.com>
2019-06-13 12:36:55 -0300

    removed lodash dependency

--- src/observable.ts
+++ src/observable.ts
@@ -1,9 +1,17 @@
 import { DependencyList, useCallback, useEffect, useState } from 'react';
 import { Observable } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
-import isEqual from 'lodash.isequal';
 
 type observerFunction<T> = () => Observable<T>
+type equal = (a: any, b: any) => boolean;
+
+let isEqual: equal = (a: any, b: any) => JSON.stringify(a) === JSON.stringify(b);
+
+import('lodash.isequal')
+  .then(module => module.default)
+  .catch(() => import ('lodash').then(module => module.isEqual))
+  .catch(() => isEqual)
+  .then(fnt => isEqual = fnt)
 
 export function useObservable<T>(
   observableGenerator: observerFunction<T>,
