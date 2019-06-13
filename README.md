9d15ebfd166bcd5055302a668ab02838e298958f
Carlos André Ferrari <caferrari@gmail.com>
2019-06-13 09:14:56 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-use-observable
+
+Use observables with react hooks in an very easy way.
---
+++ src/observable.ts
@@ -0,0 +1,38 @@
+import { DependencyList, useCallback, useEffect, useState } from 'react';
+import { Observable } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
+
+type observerFunction<T> = () => Observable<T>
+
+export function useObservable<T>(
+  observableGenerator: observerFunction<T>,
+  deps: DependencyList
+): [T | undefined, any, boolean] {
+
+  const [value, setValue] = useState<T>();
+  const [error, setError] = useState();
+  const [complete, setComplete] = useState<boolean>(false);
+
+  const cb = useCallback(observableGenerator, deps)
+
+  useEffect(() => {
+    const sub = cb()
+      .pipe(
+        distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b))
+      )
+      .subscribe(
+        (data: T) => {
+          setValue(data);
+          setError(undefined);
+        },
+        (err: any) => {
+          setValue(undefined);
+          setError(err);
+        },
+        () => setComplete(true)
+      );
+    return () => sub.unsubscribe();
+  }, [cb]);
+
+  return [value, error, complete];
+}
