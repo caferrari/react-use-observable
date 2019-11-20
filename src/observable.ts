52f6390a7865905295039842c26aa947a567edfe
danieloprado <danielprado.ad@gmail.com>
2019-11-20 13:39:21 -0300

    Allow to receive initial value from a sync observable
    Signed-off-by: danieloprado <danielprado.ad@gmail.com>

--- src/observable.ts
+++ src/observable.ts
@@ -1,5 +1,5 @@
-import { DependencyList, useCallback, useEffect, useState } from 'react';
-import { Observable } from 'rxjs';
+import { DependencyList, useCallback, useEffect, useRef, useState } from 'react';
+import { Observable, Subscription } from 'rxjs';
 
 export type observerFunction<T> = () => Observable<T>;
 
@@ -11,30 +11,67 @@ export function useObservable<T>(
   observableGenerator: observerFunction<T>,
   deps: DependencyList
 ): [T | undefined, any, boolean, undefined] {
-  const [value, setValue] = useState<T>();
-  const [error, setError] = useState();
-  const [complete, setComplete] = useState<boolean>(false);
+  let initialValue: T,
+    initialError: any,
+    initialCompleted = false;
 
   const cb = useCallback(observableGenerator, deps);
+  const state = useRef({ firstRender: true, initialized: false, subscription: null as Subscription }).current;
 
-  useEffect(() => {
-    setValue(undefined);
-    setError(undefined);
-    setComplete(false);
-
-    const sub = cb().subscribe(
-      (data: T) => {
+  if (state.firstRender) {
+    state.subscription = cb().subscribe(
+      data => {
+        initialValue = data;
+        if (!state.initialized) return;
         setValue(data);
         setError(undefined);
       },
-      (err: any) => {
+      err => {
+        initialError = err;
+        if (!state.initialized) return;
+
         setValue(undefined);
         setError(err);
       },
-      () => setComplete(true)
+      () => {
+        initialCompleted = true;
+        if (!state.initialized) return;
+
+        setComplete(true);
+      }
     );
-    return () => sub.unsubscribe();
+  }
+
+  useEffect(() => {
+    if (!state.firstRender) {
+      setValue(undefined);
+      setError(undefined);
+      setComplete(false);
+
+      state.subscription && state.subscription.unsubscribe();
+      state.subscription = cb().subscribe(
+        (data: T) => {
+          setValue(data);
+          setError(undefined);
+        },
+        (err: any) => {
+          setValue(undefined);
+          setError(err);
+        },
+        () => setComplete(true)
+      );
+    }
+
+    state.firstRender = false;
+    return () => state.subscription && state.subscription.unsubscribe();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cb]);
+
+  const [value, setValue] = useState<T>(initialValue);
+  const [error, setError] = useState(initialError);
+  const [complete, setComplete] = useState<boolean>(initialCompleted);
+
+  state.initialized = true;
 
   return [value, error, complete, undefined];
 }
